4aef083271712aca8ce9ccb6993bf46f56cbc0b6
Jonas M. Miguel <jonasmmiguel@gmail.com>
2020-10-07 07:23:07 -0300

    generated adjacency matrix for reduced case

--- src/utils/geospatial.py
+++ src/utils/geospatial.py
@@ -1,5 +1,8 @@
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 from geopy.distance import geodesic
+import numpy as np
+import pandas as pd
+from itertools import combinations
 
 
 def geodistance(nodeA: str, nodeB: str, nodes_coords: Dict[str, Tuple]) -> float:
@@ -47,6 +50,47 @@ def geodistance_from_pair(nodes_pair: Tuple[str], nodes_coord: Dict[str, Tuple])
     geodistance_from_pair(nodes_pair=(nodeA, nodeB), nodes_coords) >>>  645.727618989127
 
     """
-    distance = geodistance(nodes_pair[0], nodes_pair[1], nodes_coord)
-    return distance
+    nodeA, nodeB = nodes_pair[0], nodes_pair[1]
+    return geodistance(nodeA, nodeB, nodes_coord)
 
+
+def build_distances_mx(targets: List[str], nodes_coords: Dict[str, Tuple[float]]) -> pd.DataFrame:
+    """
+
+    :param targets: list of node names being modeled
+    :param nodes_coords:
+    :return:
+
+    Example:
+
+            DE111 	    DEF0C 	    DEF08
+    DE111 	0.000000 	645.727619 	622.931654
+    DEF0C 	645.727619 	0.000000 	109.626399
+    DEF08 	622.931654 	109.626399 	0.000000
+    """
+    # unless a list of selected nodes is provided, all nodes registered are used
+    if targets is None:
+        targets = nodes_coords.keys()
+
+    # initialize distance mx
+    distances_mx = pd.DataFrame(
+        columns=targets,
+        index=targets,
+        data=np.nan,
+    )
+
+    # get all pairwise combinations of target names
+    node_pairs = list(combinations(targets, 2))
+
+    # calculate pairwise distances for upper triangle
+    for pair in node_pairs:
+        nodeA, nodeB = pair[0], pair[1]
+        distances_mx.loc[nodeA, nodeB] = geodistance_from_pair(pair, nodes_coords)
+
+    # mirror upper into lower triangle
+    distances_mx.update(distances_mx.T)  # distance B-A = distance A-B
+
+    # fill diagonal with zeroes
+    np.fill_diagonal(distances_mx.values, 0.0)  # distance A-A = 0.0
+
+    return distances_mx
